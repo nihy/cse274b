37cbf7fbae2bbe7d563c4b3e3eaffe0ef0a687aa
nihy <nihy@miamioh.edu>
2014-11-20 11:55:11 -0500

    Completed all tests, 4/4 success

--- assignment5/DataStructureTester.h
+++ assignment5/DataStructureTester.h
@@ -8,23 +8,98 @@
 #ifndef DATASTRUCTURETESTER_H_
 #define DATASTRUCTURETESTER_H_
 
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
 namespace ods {
 
 template<class T>
 class DataStructureTester {
 protected:
-  int start_time, end_time, run_time;
+  T list;
+  clock_t runtime;
 public:
   DataStructureTester();
-  virtual ~DataStructureTester();
-  int DoSequencialAdd(int start, int end, int step);
+  //virtual ~DataStructureTester();
+  int DoSequentialAdd(int start, int end, int step);
   int DoRandomAdd(int n);
-  int DoSequencialRemove(int start, int end, int step);
+  int DoSequentialRemove(int start, int end, int step);
   int DoRandomRemove(int n);
-  int DoSequencialFind(int start, int end, int step);
+  int DoSequentialFind(int start, int end, int step);
   int DoRandomFind(int n);
 
 };
+
+template<class T>
+DataStructureTester<T>::DataStructureTester() {
+  srand(time(0));
+  runtime = 0;
+}
+
+template<class T>
+int DataStructureTester<T>::DoSequentialAdd(int start, int end, int step) {
+  runtime = clock();
+  for (int i = start; i < end; i += step)
+    list.add(i);
+
+  runtime = clock() - runtime;
+  return (((float)(runtime))/CLOCKS_PER_SEC)*1000;
+}
+
+template<class T>
+int DataStructureTester<T>::DoRandomAdd(int n) {
+  runtime = clock();
+  for (int i = 0; i < n; i++) {
+    int x = rand() % 100000;
+    list.add(x);
+  }
+  runtime = clock() - runtime;
+  return (((float)(runtime))/CLOCKS_PER_SEC)*1000;
+}
+
+template<class T>
+int DataStructureTester<T>::DoSequentialRemove(int start, int end, int step) {
+  runtime = clock();
+  for (int i = start; i < end; i += step)
+    list.remove(i);
+
+  runtime = clock() - runtime;
+  return (((float)(runtime))/CLOCKS_PER_SEC)*1000;
+}
+
+template<class T>
+int DataStructureTester<T>::DoRandomRemove(int n) {
+  runtime = clock();
+  int x = rand() % 100000;
+  for (int i = 0; i < n; i++)
+    list.remove(x);
+
+  runtime = clock() - runtime;
+  return (((float)(runtime))/CLOCKS_PER_SEC)*1000;
+}
+
+template<class T>
+int DataStructureTester<T>::DoSequentialFind(int start, int end, int step) {
+  runtime = clock();
+  for (int i = start; i < end; i += step)
+    list.find(i);
+
+  runtime = clock() - runtime;
+  return (((float)(runtime))/CLOCKS_PER_SEC)*1000;
+}
+
+template<class T>
+int DataStructureTester<T>::DoRandomFind(int n) {
+  runtime = clock();
+  int x = rand() % 100000;
+  for (int i = 0; i < n; i++)
+    list.find(x);
+
+  runtime = clock() - runtime;
+  return (((float)(runtime))/CLOCKS_PER_SEC)*1000;
+}
+
 }
 
 #endif /* DATASTRUCTURETESTER_H_ */
--- assignment5/test.cpp
+++ assignment5/test.cpp
@@ -7,9 +7,125 @@
 //============================================================================
 
 #include <iostream>
+#include "ChainedHashTable.h"
+#include "DataStructureTester.h"
+#include "LinearHashTable.h"
+#include "BinarySearchTree.h"
+#include "RedBlackTree.h"
+#include "List.h"
+
 using namespace std;
+using namespace ods;
+
+void testCHash(DataStructureTester<ChainedHashTable<int> > &chashtest) {
+  int seq_add = 0, seq_rem = 0, seq_fnd = 0, rnd_add = 0, rnd_rem = 0, rnd_fnd = 0;
+
+  for (int i = 0; i < 20; i++) {
+    seq_add += chashtest.DoSequentialAdd(0, 50000, 1);
+    seq_fnd += chashtest.DoSequentialFind(0, 50000, 1);
+    seq_rem += chashtest.DoSequentialRemove(0, 50000, 1);
+    srand(0);
+    rnd_add += chashtest.DoRandomAdd(50000);
+    srand(0);
+    rnd_fnd += chashtest.DoRandomFind(50000);
+    srand(0);
+    rnd_rem += chashtest.DoRandomRemove(50000);
+  }
+
+  cout << "Average C. Hash operation run times: " << endl;
+  cout << "Seq. ADD:      " << seq_add/20 << " milliseconds" << endl;
+  cout << "Seq. FIND:     " << seq_fnd/20 << " milliseconds" << endl;
+  cout << "Seq. REMOVE:   " << seq_rem/20 << " milliseconds" << endl;
+  cout << "Random ADD:    " << rnd_add/20 << " milliseconds" << endl;
+  cout << "Random FIND:   " << rnd_fnd/20 << " milliseconds" << endl;
+  cout << "Random REMOVE: " << rnd_rem/20 << " milliseconds" << endl;
+
+}
+
+void testLHash(DataStructureTester<LinearHashTable<int> > &lhashtest) {
+  int seq_add = 0, seq_rem = 0, seq_fnd = 0, rnd_add = 0, rnd_rem = 0, rnd_fnd = 0;
+
+  for (int i = 0; i < 20; i++) {
+    seq_add += lhashtest.DoSequentialAdd(0, 50000, 1);
+    seq_fnd += lhashtest.DoSequentialFind(0, 50000, 1);
+    seq_rem += lhashtest.DoSequentialRemove(0, 50000, 1);
+    srand(0);
+    rnd_add += lhashtest.DoRandomAdd(50000);
+    srand(0);
+    rnd_fnd += lhashtest.DoRandomFind(50000);
+    srand(0);
+    rnd_rem += lhashtest.DoRandomRemove(50000);
+  }
+
+  cout << "Average L. Hash operation run times: " << endl;
+  cout << "Seq. ADD:      " << seq_add/20 << " milliseconds" << endl;
+  cout << "Seq. FIND:     " << seq_fnd/20 << " milliseconds" << endl;
+  cout << "Seq. REMOVE:   " << seq_rem/20 << " milliseconds" << endl;
+  cout << "Random ADD:    " << rnd_add/20 << " milliseconds" << endl;
+  cout << "Random FIND:   " << rnd_fnd/20 << " milliseconds" << endl;
+  cout << "Random REMOVE: " << rnd_rem/20 << " milliseconds" << endl;
+
+}
+
+void testBSTree(DataStructureTester<BinarySearchTree<BSTNode1<int>, int> > &bstest) {
+  int seq_add = 0, seq_rem = 0, seq_fnd = 0, rnd_add = 0, rnd_rem = 0, rnd_fnd = 0;
+
+  for (int i = 0; i < 20; i++) {
+    seq_add += bstest.DoSequentialAdd(0, 50000, 1);
+    seq_fnd += bstest.DoSequentialFind(0, 50000, 1);
+    seq_rem += bstest.DoSequentialRemove(0, 50000, 1);
+    srand(0);
+    rnd_add += bstest.DoRandomAdd(50000);
+    srand(0);
+    rnd_fnd += bstest.DoRandomFind(50000);
+    srand(0);
+    rnd_rem += bstest.DoRandomRemove(50000);
+  }
+
+  cout << "Average B.S. Tree operation run times: " << endl;
+  cout << "Seq. ADD:      " << seq_add/20 << " milliseconds" << endl;
+  cout << "Seq. FIND:     " << seq_fnd/20 << " milliseconds" << endl;
+  cout << "Seq. REMOVE:   " << seq_rem/20 << " milliseconds" << endl;
+  cout << "Random ADD:    " << rnd_add/20 << " milliseconds" << endl;
+  cout << "Random FIND:   " << rnd_fnd/20 << " milliseconds" << endl;
+  cout << "Random REMOVE: " << rnd_rem/20 << " milliseconds" << endl;
+
+}
+
+void testRBTree(DataStructureTester<RedBlackTree<RedBlackNode1<int>, int> > &rbtest) {
+  int seq_add = 0, seq_rem = 0, seq_fnd = 0, rnd_add = 0, rnd_rem = 0, rnd_fnd = 0;
+
+  for (int i = 0; i < 20; i++) {
+    seq_add += rbtest.DoSequentialAdd(0, 50000, 1);
+    seq_fnd += rbtest.DoSequentialFind(0, 50000, 1);
+    seq_rem += rbtest.DoSequentialRemove(0, 50000, 1);
+
+    rnd_add += rbtest.DoRandomAdd(50000);
+    rnd_fnd += rbtest.DoRandomFind(50000);
+    rnd_rem += rbtest.DoRandomRemove(50000);
+  }
+
+  cout << "Average Red/Black Tree operation run times: " << endl;
+  cout << "Seq. ADD:      " << seq_add/20 << " milliseconds" << endl;
+  cout << "Seq. FIND:     " << seq_fnd/20 << " milliseconds" << endl;
+  cout << "Seq. REMOVE:   " << seq_rem/20 << " milliseconds" << endl;
+  cout << "Random ADD:    " << rnd_add/20 << " milliseconds" << endl;
+  cout << "Random FIND:   " << rnd_fnd/20 << " milliseconds" << endl;
+  cout << "Random REMOVE: " << rnd_rem/20 << " milliseconds" << endl;
+
+}
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+  DataStructureTester<ChainedHashTable<int> > chashtest;
+  DataStructureTester<LinearHashTable<int> > lhashtest;
+  DataStructureTester<BinarySearchTree<BSTNode1<int>, int> > bstest;
+  DataStructureTester<RedBlackTree<RedBlackNode1<int>, int> > rbtest;
+
+  //testCHash(chashtest);
+  testLHash(lhashtest);
+  //testBSTree(bstest);
+  //testRBTree(rbtest);
+
+  return 0;
 }
